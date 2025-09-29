2069402b542f3ce97008b9c43dda7dd605def30a
leman Memmedli <lemanmmdli2005@gmail.com>
2025-09-29 10:33:30 +0400

    20 task completed!

--- Desktop/C++ course/Flow control/hackerRank.cpp
+++ Desktop/C++ course/Flow control/hackerRank.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
+#include <math.h>
 
 using namespace std;
 
@@ -7,16 +9,14 @@ int main()
 {
 
     // int number;
-
     // cin>>number;
-
     // if (number % 2 ==0){
     //     cout << "The number is even : " << number <<endl;
     // }else{
     //     cout << "The number not even : "<< number <<endl;;
     // }
-    
-    // // 5
+
+    // // 5 ----------------
     // int number;
     // cin >> number;
     // int a;
@@ -26,7 +26,7 @@ int main()
     // cout << "Left digit: " << a << endl;
     // cout << "Right digit: " << b << endl;
 
-    // // 6
+    // // 6 ------------------
     // int number;
     // cin>>number;
     // int a = number /10;
@@ -34,6 +34,164 @@ int main()
     // cout << "Sum of the digits: " << a+b << endl;
     // cout << "Product of the digits: " << a*b << endl;
 
+    // // 7 -------------------
+    // int number;
+    // cin >> number;
+
+    // int a=number /10;
+    // int b=number %10;
+    // int c;
+    // c=a;
+    // a=b;
+    // b=c;
+    // if (a%10==0){
+    //     cout << b << endl;
+
+    // }else{
+    //  cout << a << "" << b << endl;
+    // }
+
+    // // 8 -----------
+    // int number;
+    // cin >> number;
+    // int a=number / 100;
+    // cout << a << endl;
+
+    // // 9 ------------
+    // int number;
+    // cin >> number;
+    // int a=number % 10;
+    // int b=(number / 10) % 10;
+    // cout << "Last digit (units): " << a <<endl;
+    // cout << "Middle digit (tens): " << b <<endl;
+
+    // // 10 ---------------
+    // int number;
+    // cin >> number;
+    // int a=number % 10;
+    // int b=(number / 10) % 10;
+    // int c=number /100;
+    // cout << "Sum of the digits: " << a+b+c <<endl;
+    // cout << "Product of the digits: " << a*b*c <<endl;
+
+    // // 3 ----------------
+    // int a;
+    // int b;
+    // cin >> a >> b;
+    // int X=a;
+    // int Y=b;;
+
+    // if (a > b)
+    // {
+    //     cout << (a = X) << " " << (b = Y);
+    // }else{
+    //     cout << (a = Y) << " " << (b = X);
+    // }
+
+    // // 11 ----------------
+    // int number ;
+    // cin >> number;
+    // int a=number % 10;
+    // int b=(number / 10) % 10;
+    // int c=number /100;
+    // cout << a << b << c << endl;
+
+    // // 12 --------------------
+    // int number ;
+    // cin >> number;
+    // int a=number % 10;
+    // int b=(number / 10) % 10;
+    // int c=number /100;
+    // cout << b << a << c << endl;
+
+    // // 13--------------------
+    // int number ;
+    // cin >> number;
+    // int a=number % 10;
+    // int b=(number / 10) % 10;
+    // int c=number /100;
+    // cout << a << c << b << endl;
+
+    // // 14--------------------
+    // int number;
+    // cin >> number;
+    // int a = number % 10;
+    // int b = (number / 10) % 10;
+    // int c = number / 100;
+    // cout << b << c << a << endl;
+
+    // // 15 -----------------------
+    // int number;
+    // cin >> number;
+    // int a = number % 10;
+    // int b = (number / 10) % 10;
+    // int c = number / 100;
+    // cout << c << a << b << endl;
+
+    // // 16-------------------
+    // int number;
+    // cin >> number;
+    // int c = (number / 100)%10;
+    // cout << c << endl;
+
+    // 17-------Palindrome---------
+    // int number;
+    // cin >> number;
+    // int a = number % 10;
+    // int b = (number / 10) % 10;
+    // int c = (number / 100) % 10;
+    // int d = number / 1000;
+
+    // if (a == d && b == c)
+    // {
+    //     cout << "Palindrome" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not Palindrome" << endl;
+    // }
+
+
+    // 18-----------------------
+    // int number;
+    // cin >> number;
+    // int a = number % 10;
+    // int b = (number / 10) % 10;
+    // int d = (number / 1000) % 10;
+    // int e= number /10000;
+
+    // cout <<( (e+d) - (a+b)) << endl;
+
+
+    // 19---------------------
+    // int number;
+    // cin >> number;
+    // int a = number % 10;
+    // int b = (number / 10) % 10;
+    // int d = (number / 1000) % 10;
+    // int e= number /10000;
+
+    // if (a == e && b == d)
+    // {
+    //     cout << "Palindrome" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not Palindrome" << endl;
+    // }
+
+
+    // 20---------------------
+    // int number;
+    // cin >> number;
+    // int a = number % 10;
+    // int b = (number / 10) % 10;
+    // int c=(number /100) %10;
+    // int d = (number / 1000) % 10;
+    // int e= number /10000;
+
+    // int result=pow(a,2) + pow(b,2) + pow(c,2) + pow(d,2) + pow (e,2);
+    // cout << result << endl;
 
 
 }
