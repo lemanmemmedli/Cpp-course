af35de3a2a1961b95f6155edcf01b06dd63878fa
leman Memmedli <lemanmmdli2005@gmail.com>
2025-10-25 23:25:46 +0400

    pointers lesson completed!

--- Desktop/C++ course/Pointers/test.cpp
+++ Desktop/C++ course/Pointers/test.cpp
@@ -56,9 +56,9 @@ int main()
 
     //  // Declare and initialize pointer
     // int    *pointerI;
-    // double *pointerD; 
+    // double *pointerD;
 
-    // // Explicitly initialize with nullptr 
+    // // Explicitly initialize with nullptr
     // int *number = nullptr;
     // int *number1(nullptr);
 
@@ -73,21 +73,174 @@ int main()
 
     // cout << endl;
 
-    int a=3;
+    // int a=3;
 
-    int *p=&a;
+    // int *p=&a;
 
-    int b;
+    // int b;
 
-    p=&b;
+    // p=&b;
 
-    cout<<p<<endl;
+    // cout<<p<<endl;
 
-    int x = 45;
-    int *pointer_int = &x; 
-    int y = 47;
-    pointer_int = &y;   // assign a different address to the pointer
+    // int x = 45;
+    // int *pointer_int = &x;
+    // int y = 47;
+    // pointer_int = &y;   // assign a different address to the pointer
 
-    cout << "pointer_int:  " << pointer_int << endl;
-    cout << "*pointer_int: " << *pointer_int << endl;
+    // cout << "pointer_int:  " << pointer_int << endl;
+    // cout << "*pointer_int: " << *pointer_int << endl;
+
+    // int number = 22;   // stack
+    // int *p_number = &number;
+
+    // cout << "number:     " << number << endl;
+    // cout << "&number:    " << &number << endl;
+    // cout << "p_number:   " << p_number << endl;
+    // cout << "*p_number:  " << *p_number << endl;
+
+    // int *p_number1;   // uninitialized pointer, contains junk address
+    // int number1 = 12;
+    // p_number1 = &number1;   // make it point to a valid address
+
+    // cout << "*p_number1: " << *p_number1 << endl;
+
+    // int *p;
+    // *p=55;
+
+    // int *pointer = nullptr;
+    // pointer = new int;
+
+    // *pointer = 77; // writing into dynamically allocated memory
+
+    // delete pointer;     // after releasing it, the pointer will contain a junk address
+    // pointer = nullptr;  // so it is good practice to initialize it to null
+
+    // cout <<pointer << endl;   // 0x0 - null address
+
+    // {
+    //     int local_var = 45;           // local_var is in stack memory
+    //     int *local_ptr_var = new int; // local_ptr_var is in heap memory
+    // }
+
+    // float *p_number7(new float(27.6));
+
+    // cout << *p_number7 << endl;
+
+    // // It is possible to reuse the above pointers
+    // int *p_number5 = new int;
+    // p_number5 = new int(81); // here we are reallocatiing new memory in the heap
+    // cout << *p_number5 << endl;
+
+    // // do not forget to release the memory again
+    // delete p_number5;
+    // p_number5 = nullptr;
+
+    // int *pointer2(new int(15));
+
+    // delete pointer2;
+
+    // cout<<*pointer2<<endl;
+
+    //  // Case 3: Multiple pointers pointing to same address
+    // int *pointer_3(new int(23));
+    // int *pointer_4(pointer_3);
+
+    // // cout << "pointer_3 - " << pointer_3 << ", " << *pointer_3 << endl;
+    // // cout << "pointer_4 - " << pointer_4 << ", " << *pointer_4 << endl;
+
+    // delete pointer_3;
+    // pointer_3=nullptr;
+
+    // // Handle with std::nothrow
+    // for (int i(0); i < 100; ++i) {
+    //     // When a heap memory allocation using "new" fails,
+    //     // the "nothrow" parameter will cause it to set the value of "new" to null.
+    //     int *arr = new (std::nothrow) int[10000000];
+
+    //     if (arr != nullptr) {
+    //         cout << "Data allocated " << (i + 1) << endl;
+    //     } else {
+    //         cout << "Data allocation failed " << (i + 1) << endl;
+    //     }
+    // }
+
+    // int *pointer8=nullptr;
+
+    // delete pointer8;
+
+    // 9. Dynamically allocated arrays
+
+    // int arr[]={1,2};
+
+    // int *p=new int [5];
+
+    // delete []p;
+
+    int size = 5;
+
+    float *scores = new (nothrow) float[size];
+    int *students = new (nothrow) int[size]();
+
+    scores[0] = 1;
+    scores[1] = 2;
+    scores[2] = 3;
+
+    // cout << "sizeof(scores): " << sizeof(students) << endl;
+    // cout<< *scores<<endl;
+    // cout<< *(scores + 0)<<endl;
+    // cout<< *(scores + 1)<<endl;
+    // cout<< *(scores + 2)<<endl;
+    // cout<< *(scores + 3)<<endl;
+    // cout<< *(scores + 4)<<endl;
+
+    // for(int i=0;i<size;++i){
+    //     cout<<"value= "<<scores[i]<<": "<<*(scores +i)<<", address: "<<(scores + i)<<endl;
+    // }
+    // delete [] students;
+    // students = nullptr;
+
+    // delete [] scores;
+    // scores = nullptr;
+
+    // int levels[5]={1,2,3,4,5};
+
+    // for(auto l: levels){
+    //     cout<<"levels: "<<l<<endl;
+    // }
+
+    int *plevels = new int[5];
+
+    for (int i = 0; i < size; ++i)
+    {
+        plevels[i] = (i + 1);
+        cout << plevels[i] << " " << endl;
+    }
+
+    // const char *message={"Hello World!"};
+
+    // cout<<"Message: "<<message<<endl;
+    // cout<<"*Message: "<<*(message + 1)<<endl;
+    // cout<<"*Message: "<<*(message + 2)<<endl;
+    // cout<<"*Message: "<<*(message + 3)<<endl;
+
+    char messages[] = ("Hello world");
+    messages[0] = 'h';
+    cout << messages << endl;
+
+    int a[] = {1, 2, 3};
+
+    cout << a << endl;
+    cout << (a + 0) << endl;
+    cout << (a + 1) << endl;
+    cout << (a + 2) << endl;
+
+    cout << *a << endl;
+    cout << *(a + 0) << endl;
+    cout << *(a + 1) << endl;
+    cout << *(a + 2) << endl;
+
+    int x = 1;
+    int y = 2;
+    int z = 3;
 }
