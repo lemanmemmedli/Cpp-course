a47e601063414cc4e82a74056f1956cd4f6f6df7
leman Memmedli <lemanmmdli2005@gmail.com>
2025-12-07 10:25:18 +0400

    Class Lesson 1

--- Desktop/C++ course/L16.Classes & Objects/test.cpp
+++ Desktop/C++ course/L16.Classes & Objects/test.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
+
+hash<string>hasher;
 
 const double PI =3.141722438473493;
 
@@ -16,6 +19,65 @@ class  Cylinder {
     } 
 };
 
+
+class User {
+    private:
+    string password = "";
+    string email = "";
+
+    public:
+
+    string name ="";
+
+    void setpassword(string orginalPassword){
+        password=hasher(orginalPassword);
+    }
+    void details(){
+        cout<<name<<endl;
+        cout<<password<<endl;
+    }
+    
+};
+
+class UserInfo {
+    private:
+    string password= "";
+
+    private:
+    string email = "";
+
+    public:
+
+    string name="";
+    void setpassword(string orginalPassword){
+        password=hasher(orginalPassword);
+    }
+    public:
+    void setemail(string orginalemail){
+        email=orginalemail;
+    }
+
+    void Ddetails(){
+        cout<<name<<endl;
+        cout<<password<<endl;
+        cout<<email<<endl;
+    }
+};
+
+class Cube{
+    public:
+    int height=1;
+    int b=1;
+    int c=1;
+
+
+    public:
+
+    int volume(){
+        return height*b*c;
+    }
+};
+
 int main(){
     
     Cylinder Cylinder1;
@@ -23,4 +85,34 @@ int main(){
     Cylinder1.height = 10;
 
     cout<<Cylinder1.volume()<<endl;
+// ------------------------
+    User first;
+
+    first.setpassword("Hello!");
+
+    first.name ="Jhon";
+
+    first.details();
+
+//    ------------------------------
+    UserInfo second;
+
+    second.setpassword("Salam!");
+    second.setemail("Salam@gmail.com");
+
+    second.name="Jane";
+
+    second.Ddetails();
+    // ---------------------------
+    Cube voll;
+
+    voll.height=12;
+    voll.b=5;
+    voll.c=15;
+
+    cout<<voll.volume();
+
+
+
+
 }
